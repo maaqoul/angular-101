01592a1decb574d553f39d84a9d7f72e03b732f6
maaqoul <hicham.maaqoul@gmail.com>
2021-07-01 10:22:05 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>
+    Hi!
+    <div>`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sqli-angular-training';
+}
