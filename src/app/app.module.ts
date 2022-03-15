d7c7b1a1dc00f55bc509cc6a4f0cb54c36f451e8
maaqoul <hicham.maaqoul@gmail.com>
2022-03-15 21:15:16 +0100

    rendering flows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    // Angular Modules
-    BrowserModule,
-
-  ],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
